7a81245a15c98ffa2f57d7afe14cb200e07c6284
Vishal Talwar <vtalwar.w@gmail.com>
2025-08-02 13:43:55 -0700

    refactors logger module for lang updates

--- logger.go
+++ logger.go
@@ -1,6 +1,10 @@
 package logger
 
 import (
+	"context"
+	"errors"
+	"io"
+	"log/slog"
 	"os"
 	"path/filepath"
 
@@ -10,41 +14,113 @@ import (
 )
 
 const DEFAULT_LOG_FILE_PATH = "logs/app.log"
-const DEFAULT_LOG_LEVEL = zapcore.DebugLevel
+const NO_LOGGER_FOUND = "no logger found in context"
 
-type AppLogger interface {
-	Info(msg string, fields ...interface{})
-	Warn(msg string, fields ...interface{})
-	Debug(msg string, fields ...interface{})
-	Error(msg string, fields ...interface{})
-	Panic(msg string, fields ...interface{})
-	Fatal(msg string, fields ...interface{})
+var ErrNoLoggerInContext = errors.New(NO_LOGGER_FOUND)
+
+type LoggerContextKey string
+
+const contextLoggerKey LoggerContextKey = "ContextLoggerKey"
+
+type Logger interface {
+	Debug(msg string, fields ...any)
+	Info(msg string, fields ...any)
+	Warn(msg string, fields ...any)
+	Error(msg string, fields ...any)
 }
 
-type appLogger struct {
-	*zap.Logger
-	config *AppLoggerConfig
+// WithLogger returns a new context with the given logger.
+func WithLogger(ctx context.Context, logger Logger) context.Context {
+	return context.WithValue(ctx, contextLoggerKey, logger)
 }
 
-type AppLoggerConfig struct {
-	FilePath string
-	Name     string
-	Level    zapcore.Level
+// LoggerFromContext retrieves the logger from context.
+// If none is found, returns a fallback logger.
+func LoggerFromContext(ctx context.Context) (Logger, error) {
+	logger, ok := ctx.Value(contextLoggerKey).(Logger)
+	if !ok {
+		return nil, ErrNoLoggerInContext
+	}
+	return logger, nil
 }
 
-func NewAppLogger(config *AppLoggerConfig) *appLogger {
-	logLevel := DEFAULT_LOG_LEVEL
-	filePath := DEFAULT_LOG_FILE_PATH
+func GetSlogLogger() *slog.Logger {
+	// Initialize log level to Info
+	logLevel := &slog.LevelVar{}
+	logLevel.Set(slog.LevelInfo)
 
-	if config != nil {
-		if config.FilePath != "" {
-			filePath = config.FilePath
-		}
-
-		logLevel = config.Level
+	// Set log level to Debug if running in local infrastructure
+	if os.Getenv("INFRA") == "local" {
+		logLevel.Set(slog.LevelDebug)
 	}
 
+	// Setup slog handler options. TODO update for log formatting
+	opts := &slog.HandlerOptions{
+		Level: logLevel,
+	}
+
+	// Using TextHandler. TODO use JsonHandler for structured logging
+	handler := slog.NewTextHandler(os.Stdout, opts)
+
+	l := slog.New(handler)
+	slog.SetDefault(l)
+
+	return l
+}
+
+func GetSlogMultiLogger(dir string) *slog.Logger {
+	filePath := DEFAULT_LOG_FILE_PATH
+	if dir != "" {
+		filePath = filepath.Join(dir, filePath)
+	}
+
+	// Initialize log level to Info
+	logLevel := &slog.LevelVar{}
+	logLevel.Set(slog.LevelInfo)
+
+	// Set log level to Debug if running in local infrastructure
+	if os.Getenv("INFRA") == "local" {
+		logLevel.Set(slog.LevelDebug)
+	}
+
+	// lumberjack writer for log rotation
+	logWriter := &lumberjack.Logger{
+		Filename:   filePath,
+		MaxSize:    100, // megabytes
+		MaxBackups: 5,
+		MaxAge:     28,   // days
+		Compress:   true, // compress rotated logs
+	}
+
+	// MultiWriter for logs in both file & console
+	multiWriter := io.MultiWriter(os.Stdout, logWriter)
+
+	// Setup slog handler options. TODO update for log formatting
+	opts := &slog.HandlerOptions{
+		Level: logLevel,
+	}
+
+	// Using TextHandler. TODO use JsonHandler for structured logging
+	handler := slog.NewTextHandler(multiWriter, opts)
+
+	l := slog.New(handler)
+	slog.SetDefault(l)
+
+	return l
+}
+
+func GetZapLogger(dir, namedAs string) *zap.Logger {
+	filePath := DEFAULT_LOG_FILE_PATH
+	if dir != "" {
+		filePath = filepath.Join(dir, filePath)
+	}
+
+	logLevel := zapcore.InfoLevel
 	cfg := zap.NewProductionEncoderConfig()
+	if os.Getenv("INFRA") == "local" {
+		logLevel = zapcore.DebugLevel
+		cfg = zap.NewDevelopmentEncoderConfig()
+	}
 	cfg.EncodeTime = zapcore.ISO8601TimeEncoder
 
 	fileEncoder := zapcore.NewJSONEncoder(cfg)
@@ -61,121 +137,6 @@ func NewAppLogger(config *AppLoggerConfig) *appLogger {
 		zapcore.NewCore(fileEncoder, writer, logLevel),
 		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), logLevel),
 	)
-	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
-	if config.Name != "" {
-		logger = logger.Named(config.Name)
-	}
-
-	return &appLogger{
-		Logger: logger,
-		config: config,
-	}
-}
-
-func NewAppZapLogger(config *AppLoggerConfig) *zap.Logger {
-	logLevel := DEFAULT_LOG_LEVEL
-	filePath := DEFAULT_LOG_FILE_PATH
-
-	if config != nil {
-		if config.FilePath != "" {
-			filePath = config.FilePath
-		}
-
-		logLevel = config.Level
-	}
-
-	cfg := zap.NewProductionEncoderConfig()
-	cfg.EncodeTime = zapcore.ISO8601TimeEncoder
-
-	fileEncoder := zapcore.NewJSONEncoder(cfg)
-	consoleEncoder := zapcore.NewConsoleEncoder(cfg)
-
-	writer := zapcore.AddSync(&lumberjack.Logger{
-		Filename:   filePath,
-		MaxSize:    10, // megabytes
-		MaxBackups: 3,
-		MaxAge:     28, // days
-	})
-
-	core := zapcore.NewTee(
-		zapcore.NewCore(fileEncoder, writer, logLevel),
-		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), logLevel),
-	)
-	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
-	if config.Name != "" {
-		logger = logger.Named(config.Name)
-	}
-
-	return logger
-}
-
-func NewTestAppLogger(dir string) *appLogger {
-	logLevel := DEFAULT_LOG_LEVEL
-	filePath := DEFAULT_LOG_FILE_PATH
-
-	if dir != "" {
-		filePath = filepath.Join(dir, DEFAULT_LOG_FILE_PATH)
-	}
-
-	logCfg := AppLoggerConfig{
-		Level:    logLevel,
-		FilePath: filePath,
-	}
-
-	cfg := zap.NewDevelopmentEncoderConfig()
-	cfg.EncodeTime = zapcore.ISO8601TimeEncoder
-
-	fileEncoder := zapcore.NewJSONEncoder(cfg)
-	consoleEncoder := zapcore.NewConsoleEncoder(cfg)
-
-	writer := zapcore.AddSync(&lumberjack.Logger{
-		Filename:   logCfg.FilePath,
-		MaxSize:    10, // megabytes
-		MaxBackups: 3,
-		MaxAge:     28, // days
-	})
-
-	core := zapcore.NewTee(
-		zapcore.NewCore(fileEncoder, writer, logCfg.Level),
-		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), logCfg.Level),
-	)
-	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel)).Named("test")
-	return &appLogger{
-		Logger: logger,
-		config: &logCfg,
-	}
-}
-
-func NewTestAppZapLogger(dir string) *zap.Logger {
-	logLevel := DEFAULT_LOG_LEVEL
-	filePath := DEFAULT_LOG_FILE_PATH
-
-	if dir != "" {
-		filePath = filepath.Join(dir, DEFAULT_LOG_FILE_PATH)
-	}
-
-	logCfg := AppLoggerConfig{
-		Level:    logLevel,
-		FilePath: filePath,
-	}
-
-	cfg := zap.NewDevelopmentEncoderConfig()
-	cfg.EncodeTime = zapcore.ISO8601TimeEncoder
-
-	fileEncoder := zapcore.NewJSONEncoder(cfg)
-	consoleEncoder := zapcore.NewConsoleEncoder(cfg)
-
-	writer := zapcore.AddSync(&lumberjack.Logger{
-		Filename:   logCfg.FilePath,
-		MaxSize:    10, // megabytes
-		MaxBackups: 3,
-		MaxAge:     28, // days
-	})
-
-	core := zapcore.NewTee(
-		zapcore.NewCore(fileEncoder, writer, logCfg.Level),
-		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), logCfg.Level),
-	)
-	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel)).Named("test")
+	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel)).Named(namedAs)
 	return logger
 }
